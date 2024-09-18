8b62c672516cea1766526e10e20ae17dc37de19e
TamaraFdez <tamarafdezosmi23@gmail.com>
2024-09-18 23:59:29 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,15 @@ export default [
       'semi': ['error', 'never'], 
       'no-unused-vars': ['warn'], 
       'no-undef': ['error'],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+          'error', 'always'
+      ],
+      'arrow-spacing': [
+          'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
     }
   },
   {
